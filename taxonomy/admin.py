d43f4b669c31ab46230a341c196c2be6a816b420
mfrasca <mario@anche.no>
2019-04-20 18:11:41 -0500

    allowing for bulk editing in the admin

--- taxonomy/admin.py
+++ taxonomy/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Taxon
+
+#######################################################
+class TaxonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'rank', 'epithet', 'authorship', 'year', 'parent', 'accepted', )
+    list_editable = ('epithet', 'authorship', 'year', )
+
+admin.site.register(Taxon, TaxonAdmin)
