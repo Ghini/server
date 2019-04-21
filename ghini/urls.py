aedbc81343da2b97c1702ae6581f19ec0eb624ba
mfrasca <mario@anche.no>
2019-04-21 09:10:20 -0500

    adding favicon

--- ghini/urls.py
+++ ghini/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('taxonomy/', include('taxonomy.urls'))
+    path('taxonomy/', include('taxonomy.urls')),
+    path("favicon.ico", favicon_view),
 ]
