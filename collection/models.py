002bbbbd7c224aa4cdd3e53680a12368acae569d
mfrasca <mario@anche.no>
2019-04-22 11:18:46 -0500

    connecting accession-taxon via verifications

--- collection/models.py
+++ collection/models.py
@@ -47,7 +47,7 @@ recvd_type_values = (
 # Create your models here.
 
 class Accession(models.Model):
-    taxon = models.ForeignKey(Taxon, related_name='accessions', on_delete=models.PROTECT)
+    taxa = models.ManyToManyField(Taxon, related_name='accessions', through='Verification')
     qualifier = models.CharField(blank=True, null=True, max_length=8, choices=QUALIFIERS)
 
     accessioned_date = models.DateField(blank=True, null=True)
@@ -60,5 +60,27 @@ class Accession(models.Model):
 
 
 class Contact(models.Model):
-    pass
+    name = models.CharField(max_length=64, default='')
 
+    def __str__(self):
+        return self.name
+
+
+VERIFICATION_LEVELS = (
+    ('0', 'The name of the record has not been checked by any authority.'),
+    ('1', 'The name of the record determined by comparison with other named plants.'),
+    ('2', 'The name of the record determined by a taxonomist or by other competent persons using herbarium and/or library and/or documented living material.'),
+    ('3', 'The name of the plant determined by taxonomist engaged in systematic revision of the group.'),
+    ('4', 'The record is part of type gathering or propagated from type material by asexual methods'),
+    )
+
+
+class Verification(models.Model):
+    accession = models.ForeignKey(Accession, blank=False, related_name='verifications', on_delete=models.CASCADE)
+    taxon = models.ForeignKey(Taxon, blank=False, related_name='verifications', on_delete=models.PROTECT)
+    level = models.CharField(blank=False, null=False, max_length=1, choices=VERIFICATION_LEVELS, default='0')
+    contact = models.ForeignKey(Contact, blank=False, related_name='verifications', on_delete=models.PROTECT)
+    date = models.DateField(blank=False, null=False)
+
+    def __str__(self):
+        return "{} says {} is a {} - level {}".format(self.contact.name, self.accession.code, self.taxon.show(), self.level)
