3421fb7c8d02e0f3d8a22ce5a65047528f804dfd
mfrasca <mario@anche.no>
2019-04-30 17:12:52 -0500

    work in progress, moving towards select2

--- taxonomy/admin.py
+++ taxonomy/admin.py
@@ -7,11 +7,14 @@ from .models import Taxon, Rank
 class TaxonAdmin(admin.ModelAdmin):
     list_display = ('id', 'rank', 'epithet', 'authorship', 'year', 'parent', 'accepted', )
     list_editable = ('epithet', 'authorship', 'year', )
+    autocomplete_fields = ['rank', 'parent', 'accepted']
+    search_fields = ['epithet']
 
 #######################################################
 class RankAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'short', 'show_as', )
     list_editable = ('name', 'short', 'show_as', )
+    search_fields = ['name']
 
 admin.site.register(Taxon, TaxonAdmin)
 admin.site.register(Rank, RankAdmin)
