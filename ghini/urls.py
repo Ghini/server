9aeb9e1ec8923acbed2bae36775af5a178a275d5
mfrasca <mario@anche.no>
2019-05-05 18:55:40 -0500

    unrelated, just adding a 'home' view

--- ghini/urls.py
+++ ghini/urls.py
@@ -1,30 +1,23 @@
 """ghini URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import RedirectView
 
 favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
+root_view = RedirectView.as_view(url='/browse/', permanent=True)
 
 urlpatterns = [
+    # admin
     path('admin/', admin.site.urls),
+    # include
     path('taxonomy/', include('taxonomy.urls')),
     path('collection/', include('collection.urls')),
     path('garden/', include('garden.urls')),
     path('browse/', include('browse.urls')),
-    path("favicon.ico", favicon_view),
     path('select2/', include('django_select2.urls')),
+    # root redirect views
+    path("favicon.ico", favicon_view, name='favicon'),
+    path('', root_view, name='home'),
 ]
