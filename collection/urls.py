8b604b9bde544bffdb43e1f3c7ebdb38e36f0a3f
mfrasca <mario@anche.no>
2019-05-05 14:39:34 -0500

    related to #16 - we can activate forms

--- collection/urls.py
+++ collection/urls.py
@@ -8,19 +8,19 @@ from .forms import AccessionForm, VerificationForm, ContactForm
 urlpatterns = [
     # accession
     path("accession/", AccessionList.as_view(), name="accession-list"),
+    path("accession//form/", AccessionForm.as_view(), name="accession-post-form"),
+    path("accession/<int:code>/form/", AccessionForm.as_view(), name="accession-put-form"),
     path("accession/<str:code>/", AccessionDetail.as_view(), name="accession"),
     path("accession/<str:code>/infobox/", AccessionInfobox.as_view(), name="accession-infobox"),
-    path("accession/form/", AccessionForm.as_view(), name="accession-post-form"),
-    path("accession/<int:code>/form/", AccessionForm.as_view(), name="accession-put-form"),
     # verification
     path("accession/<str:accession_code>/verification/", VerificationList.as_view(), name="verification-list"),
     path("accession/<str:accession_code>/verification/<int:seq>/", VerificationDetail.as_view(), name="verification"),
-    path("accession/<str:accession_code>/verification/form/", VerificationForm.as_view(), name="verification-post-form"),
+    path("accession/<str:accession_code>/verification//form/", VerificationForm.as_view(), name="verification-post-form"),
     path("accession/<str:accession_code>/verification/<int:seq>/form/", VerificationForm.as_view(), name="verification-put-form"),
     # contact
     path("contact/", ContactList.as_view(), name="contact-list"),
     path("contact/<int:pk>/", ContactDetail.as_view(), name="contact"),
     path("contact/<int:pk>/infobox/", ContactInfobox.as_view(), name="contact-infobox"),
-    path("contact/form/", ContactForm.as_view(), name="contact-post-form"),
+    path("contact//form/", ContactForm.as_view(), name="contact-post-form"),
     path("contact/<int:pk>/form/", ContactForm.as_view(), name="contact-put-form"),
 ]
--- taxonomy/urls.py
+++ taxonomy/urls.py
@@ -6,6 +6,8 @@ from .forms import TaxonForm
 
 urlpatterns = [
     path("taxon/", TaxonList.as_view(), name="taxon-list"),
+    path("taxon//form/", TaxonForm.as_view(), name="taxon-post-form"),
+    path("taxon/<int:pk>/form/", TaxonForm.as_view(), name="taxon-put-form"),
     path("taxon/<int:pk>/", TaxonDetail.as_view(), name="taxon-detail"),
     path("taxon/<int:pk>/infobox/", TaxonInfobox.as_view(), name="taxon-infobox"),
     path("rank/", RankList.as_view(), name="rank-list"),
@@ -13,7 +15,5 @@ urlpatterns = [
     #path("rank/<int:pk>/infobox/", RankInfobox.as_view(), name="rank-infobox"),
     #path("taxon/<int:pk>/subtaxon/", SubTaxa.as_view(), name="subtaxon-list"),
 
-    path("taxon/form/", TaxonForm.as_view(), name="taxon-post-form"),
-    path("taxon/<int:pk>/form/", TaxonForm.as_view(), name="taxon-put-form"),
 
 ]
