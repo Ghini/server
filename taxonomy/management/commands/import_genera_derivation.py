e1003e15c9bd18f9a9db0cbc0377f14593877971
mfrasca <mario@anche.no>
2019-05-29 21:30:38 -0500

    better initialization, much faster. #77
    still better to keep it apart, but it's doable this way.

--- taxonomy/management/commands/import_genera_derivation.py
+++ taxonomy/management/commands/import_genera_derivation.py
@@ -2,64 +2,79 @@ from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from django.utils.crypto import get_random_string
 
+from taxonomy.models import Taxon, Rank
 
 class Command(BaseCommand):
     help = 'import full genera derivation'
 
-    def add_arguments(self, parser):
-        parser.add_argument('--filter-genera', type=str, default=None,
-                            help='location of genus.txt from ghini.desktop csv export')
+    def next_id(self):
+        return next(self._next_id())
+
+    def _next_id(self):
+        while True:
+            self.max_id += 1
+            yield self.max_id
 
     def handle(self, *args, **kwargs):
-        genus_txt = kwargs['filter_genera']
-        accept_genera = set()
-        if genus_txt:
-            with open(genus_txt) as f:
-                for line in f.readlines():
-                    genus, _ = line.split(',', 1)
-                    accept_genera.add(genus)
         # We can import the model directly
-        from taxonomy.models import Taxon, Rank
         rank = {name: Rank.objects.get(name=name)
                 for name in ['genus', 'subtribus', 'tribus', 'subfamilia', 'familia']}
-        with open('taxonomy/management/data/genus-to-family.txt') as f:
-            bulk = []
-            counting = 1
-            while True:
-                orig_epithet = next(f).strip()
-                if not orig_epithet:
-                    break
-                lines = []
-                while True:
-                    line = next(f).strip()
-                    if not line:
-                        break
-                    lines.append(line.split(':'))
-                if accept_genera and orig_epithet not in accept_genera:
-                    continue
-                orig, _ = Taxon.objects.get_or_create(epithet=orig_epithet, rank=rank['genus'])
-                for_sake_of_logging = [orig.epithet]
-                acc_rank, acc_epithet = lines.pop()
-                if acc_epithet != orig_epithet:
-                    accepted, _ = Taxon.objects.get_or_create(epithet=acc_epithet, rank=rank[acc_rank])
-                    orig.accepted = accepted
-                    bulk.append(orig)
-                    orig = accepted
-                    for_sake_of_logging.append("-->{}".format(orig.epithet))
-                while lines:
-                    parent_rank, parent_epithet = lines.pop()
-                    parent, is_new_parent = Taxon.objects.get_or_create(epithet=parent_epithet, rank=rank[parent_rank])
-                    if orig.parent != parent:
-                        orig.parent = parent
-                        bulk.append(orig)
-                    orig = parent
-                    for_sake_of_logging.append(orig.epithet)
-                    if not is_new_parent:
-                        break
+        with open('taxonomy/management/data/one-step-derivation.txt') as f:
+            doing = 'familia'
+            self.bulk_update = []
+            self.bulk_create = []
+            counting = 0
+            from django.db.models import Max
+            self.max_id = Taxon.objects.aggregate(Max('id'))['id__max'] + 300
+            while True:  # first loop: link to parent
+                line = next(f).strip()
                 counting += 1
-                if len(bulk) > 250:
-                    self.stdout.write("\r{:>5} {}".format(counting, '... updating ...'), ending=" ")
-                    Taxon.objects.bulk_update(bulk, ['parent', 'accepted'])
-                    bulk.clear()
-                self.stdout.write("\r{:>5} {}".format(counting, ", ".join(for_sake_of_logging)), ending=" ")
+                if not line:
+                    self.flush_bulk(0)
+                    break  # let's now do genus synonyms
+                prank, pepithet, crank, cepithet = line.split(',')
+                try:
+                    parent = Taxon.objects.get(epithet=pepithet, rank=rank[prank])
+                except Taxon.DoesNotExist:
+                    parent = Taxon.objects.create(epithet=pepithet, rank=rank[prank], id=self.next_id())
+                    parent.save()
+                try:
+                    child = Taxon.objects.get(epithet=cepithet, rank=rank[crank])
+                    if child.parent != parent:
+                        child.parent = parent
+                        self.bulk_update.append(child)
+                except Taxon.DoesNotExist:
+                    child = Taxon(epithet=cepithet, rank=rank[crank], parent=parent, id=self.next_id())
+                    self.bulk_create.append(child)
+                self.flush_bulk()
+                self.stdout.write("\r{:>5} {}".format(counting, line), ending=" ")
+            while True:  # second loop: accepted
+                line = next(f).strip()
+                counting += 1
+                if not line:
+                    self.flush_bulk(0)
+                    break  # we're finished
+                this, that = line.split(',')
+                that = Taxon.objects.get(epithet=that, rank=rank['genus'])
+                try:
+                    this = Taxon.objects.get(epithet=this, rank=rank['genus'])
+                    this.parent = that.parent
+                    this.accepted = that
+                    self.bulk_update.append(this)
+                except Taxon.DoesNotExist:
+                    this = Taxon(epithet=this, rank=rank['genus'], id=self.next_id(), parent=that.parent, accepted=that)
+                    self.bulk_create.append(this)
+                self.flush_bulk()
+                self.stdout.write("\r{:>5} {}".format(counting, line), ending=" ")
         self.stdout.write("\rdone")
+
+    def flush_bulk(self, threshold=375):
+        if len(self.bulk_update) > threshold:
+            self.stdout.write("\r{}".format('... updating ...'), ending=" ")
+            Taxon.objects.bulk_update(self.bulk_update, ['parent', 'accepted'])
+            self.bulk_update.clear()
+        if len(self.bulk_create) > threshold:
+            self.stdout.write("\r{}".format('... creating ...'), ending=" ")
+            Taxon.objects.bulk_create(self.bulk_create)
+            self.bulk_create.clear()
+        
