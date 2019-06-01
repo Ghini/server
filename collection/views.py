b16d258585477b779eaca52504cd174238dc1db4
mfrasca <mario@anche.no>
2019-06-01 11:23:50 -0500

    we're now showing 'verifications', not 'taxa'

--- collection/views.py
+++ collection/views.py
@@ -64,10 +64,12 @@ class AccessionInfobox(AccessionDetail):
             result['__detail_url__'] = reverse('accession-detail', args=[obj.code])
             result['__shows_as__'] = "%s" % obj
             result['code'] = obj.code
-            result['taxa'] = ('link', [("{} ({})".format(v.taxon.identify(), v.taxon.family),
-                                        "taxon where epithet='{0.epithet}' and rank.name='{0.rank.name}'".format(v.taxon),
-                                        '\n'.join(["{0.contact.name} {0.level}".format(v)] + v.taxon.derivation_up_to_order))
-                                       for v in obj.verifications.all()])
+            result['verifications'] = (
+                'link', [("{} ({})".format(v.taxon.identify(), v.taxon.family),
+                          "taxon where epithet='{0.epithet}' and rank.name='{0.rank.name}'".format(v.taxon),
+                          '\n'.join(["{0.contact.name} {0.level}".format(v)] + v.taxon.derivation_up_to_order))
+                         for v in obj.verifications.all()])
+            result['taxa'] = None
             result['received_quantity'] = obj.received_quantity
             if obj.source is not None:
                 result['source'] = ('link',
